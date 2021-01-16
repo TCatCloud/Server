d437cc1535afe817b0f93981731f346c2f12dd49
Moexin <moexin@qq.com>
2021-01-16 19:07:27 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -5,8 +5,8 @@ echo '正在删除后端'
 rm -rf /root/TCat
 echo '正在下载后端'
 while true;do wget -P /root/TCat -T 15 -c -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat && break;done
-while true;do wget -P /root/TCat -T 15 -c -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip && break;done
-unzip /root/TCat/v2ray-linux-64.zip -d /root/TCat > /dev/null
+while true;do wget -P /root/TCat -T 15 -c -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat.zip && break;done
+unzip /root/TCat/TCat.zip -d /root/TCat > /dev/null
 chmod +x /root/TCat/*
 echo '正在启动后端'
 systemctl start TCat
