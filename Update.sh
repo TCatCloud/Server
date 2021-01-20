663fe0a463b4992abc5d5cb4164503b57c811913
Moexin <moexin@qq.com>
2021-01-20 17:05:04 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -4,8 +4,8 @@ systemctl stop TCat
 echo '正在删除后端'
 rm -rf /root/TCat
 echo '正在下载后端'
-while true;do wget -P /root/TCat -T 15 -c -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat && break;done
-while true;do wget -P /root/TCat -T 15 -c -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat.zip && break;done
+wget -P /root/TCat https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat
+wget -P /root/TCat https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat.zip
 unzip /root/TCat/TCat.zip -d /root/TCat > /dev/null
 chmod +x /root/TCat/*
 echo '正在启动后端'
