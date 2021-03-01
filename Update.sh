e6782f0971591be530559b93b14aab65f7afe890
Moexin <moexin@qq.com>
2021-03-01 19:22:38 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-echo '正在停止后端'
 systemctl stop TCat
-echo '正在删除后端'
 rm -rf /root/TCat
-echo '正在下载后端'
-wget -P /root/TCat -T 15 -t 30 -c -nv https://raw.githubusercontent.com/TCatCloud/Server/master/TCat
-wget -P /root/TCat -T 15 -t 30 -c -nv https://raw.githubusercontent.com/TCatCloud/Server/master/TCat.zip
+wget -P /root/TCat -T 15 -t 30 -c -q https://raw.githubusercontent.com/TCatCloud/Server/master/TCat
+wget -P /root/TCat -T 15 -t 30 -c -q https://raw.githubusercontent.com/TCatCloud/Server/master/TCat.zip
 unzip /root/TCat/TCat.zip -d /root/TCat > /dev/null
 chmod +x /root/TCat/*
-echo '正在启动后端'
 systemctl start TCat
+sleep 5
+systemctl status TCat
